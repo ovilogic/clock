6f9830f0ac56fc671b2b5df1fc115f0cd742b8a4
ovilogic <ovidiu.opreah@yahoo.com>
2022-04-12 23:27:38 +0100

    beginnings of a timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './style/app.css'
 
 function App() {
+  
+  const [current, setCurrent] = useState('session')
+  const [session, setSession] = useState(25)
+  const [min, setMin] = useState(0)
+  const [sec, setSec] = useState(0)
+  
+  
+
+  const newTime = new Date()
+  
+  // newTime.setMinutes(newTime.getMinutes() + session)
+  // let n = newTime.getTime()
+ 
+
+  // const now = new Date().getTime()
+
+  // let difference = n - now
+  
+
+  // console.log(difference)
+
+
+  // // setInterval(() => {
+ 
+
+  // //   var minutes = Math.floor(difference / (1000 * 60))
+   
+  // //   setMin(minutes)
+    
+  //   }, 60000)
+
+  // setInterval(() => {
+ 
+   
+  //   var seconds = Math.floor((difference % (1000 * 60)) / 1000)
+
+  //   setSec(seconds)
+  // }, 1000)
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>25 + 5 Clock</h1>
+      <div className="panel">
+        <div className='break'>
+          <h2>break length</h2>
+          <div className='functions'></div>
+        </div>
+        <div className='session'>
+          <h2>session length</h2>
+          <div className='functions'></div>
+        </div>
+      </div>
+      <div className="display">
+        <h2>{current}</h2>
+        <div className='timer'>
+          {min}:{sec}
+        </div>
+      </div>
+      <div className="controls">
+
+      </div>
+      <div className="footer">
+
+      </div>
     </div>
   );
 }
