73f6bf7e60a259bcccfab19998dc52de64ae95cd
ovilogic <ovidiu.opreah@yahoo.com>
2022-04-15 14:09:22 +0100

    buttons play and pause are now fully functional, as well as reset

--- src/App.js
+++ src/App.js
@@ -18,32 +18,43 @@ function App() {
   const [sec, setSec] = useState('00')
   const [on, setOn] = useState(0)
   const clock = useRef('')
+  const [dist, setDist] = useState(0)
+  const [dt, setDt] = useState()
   
-  
- 
-  const d = new Date()
 
-  
-  d.setMinutes(d.getMinutes() + session)
-  // console.log(d)
-  var countDownDate = d.getTime();
-
-
-  // Update the count down every 1 second
-  
   var x
   
 
   useEffect(() => {
+
+    // You don't want variables outside useEffect because they will be updated with each
+    // new render. Had d been defined outside, it would create a new time 0 (d) with each 
+    // new render. This means that what gets passed to the on == 2 section is a new d, not
+    // the old one, that the setInterval() started with. So the 
+
+    var d = new Date()
+
+
+    if (on == 1) {
+      
+      console.log(d)
+      d.setMinutes(d.getMinutes() + session)
+      console.log(d, 'minutes added')
+      var countDownDate = d.getTime();
+
+    }
     x = setInterval(() => {
-       
+   
       if (on == 1) {
+        
+      console.log('on = 1')
       
       
       var now = new Date().getTime();
 
       // Find the distance between now and the count down date
       var distance = countDownDate - now;
+      console.log('one: distance: ', distance)
 
       // Time calculations for days, hours, minutes and seconds
         
@@ -65,30 +76,30 @@ function App() {
       clock.current.innerText = minutes + ':' + seconds
       setMin(minutes)
       setSec(seconds)
+      setDist(distance)
 
+      console.log('on = 1', 'countDwn exiting 1', countDownDate, 'and distance ', distance)
       }
 
       else if (on == 2) {
-        const mid = new Date()
-        // console.log('one', mid)
-        mid.setMinutes(mid.getMinutes() + Number(min))
-        // console.log('two', mid)
-
-        mid.setSeconds(mid.getSeconds() + Number(sec))
-        // console.log('final', mid)
+        console.log('d entering 2 ', d, 'distance is', dist)
+        countDownDate = d.getTime() + dist
         
-        countDownDate = mid.getTime()
-        // console.log('mid milisecs', countDownDate)
+     
       }
 
       else if (on == 3) {
+        console.log('d entering 3 ', d, 'distance is', dist)
+        countDownDate = d.getTime() + dist
+        
     
           console.log('on is 3', countDownDate)
           
           var now = new Date().getTime();
+          console.log('third', now)
     
           // Find the distance between now and the count down date
-          var distance = countDownDate - now;
+          let distance = countDownDate - now;
     
           // Time calculations for days, hours, minutes and seconds
             
@@ -110,6 +121,7 @@ function App() {
           clock.current.innerText = minutes + ':' + seconds
           setMin(minutes)
           setSec(seconds)
+          setDist(distance)
         }
 
         }, 1000);
@@ -137,7 +149,7 @@ function App() {
           <div className='functions'>
           <BsArrowDownCircleFill className='dArrow' onClick={() => setBrk(brk - 1)}
              id='break-decrement' />
-          <div className='brk-session'>
+          <div className='brk-session' id='break-length'>
             {brk}
           </div>
           <BsArrowUpCircleFill className='uArrow' onClick={() => setBrk(brk + 1)}
@@ -153,7 +165,8 @@ function App() {
               clock.current.innerText = session - 1 + ':00'
             }}
               id='session-decrement' />
-            <div className='brk-session'>{session}</div>
+            <div className='brk-session'
+              id='session-length'>{session}</div>
             <BsArrowUpCircleFill className='uArrow' onClick={() => {
               setSession(session + 1);
               clock.current.innerText = session + 1 + ':00'
@@ -163,13 +176,16 @@ function App() {
         </div>
       </div>
       <div className="display">
-        <h2>{current}</h2>
+        <h2 id='timer-label'>{current}</h2>
         <div className='timer'>
-          <div className='current' ref={clock}>{min}:{sec}</div>
+          <div className='current'
+            id='time-left' ref={clock}>{min}:{sec}</div>
         </div>
       </div>
       <div className="controls">
-        <FaPlay className='ctrl' onClick={() => {
+        <FaPlay className='ctrl'
+          id='start_stop' onClick={() => {
+          
           if (on == 0) {
             setOn(on + 1);
           }
@@ -179,13 +195,31 @@ function App() {
           else if (on == 2) {
             setOn(3)
           }
+          else if (on == 3) {
+            setOn(2)
+          }
           
           }}/>
-        <FaPause className='ctrl' />
-        <MdReplayCircleFilled className='ctrl' onClick={() => {
+        <FaPause className='ctrl' 
+          onClick={() => {
+          if (on == 1) {
+              setOn(on + 1)
+            }
+            else if (on == 2) {
+              setOn(3)
+            }
+            else if (on == 3) {
+              setOn(2)
+            }
+            }} />
+        <MdReplayCircleFilled className='ctrl'
+          id='reset' onClick={() => {
               setSession(25);
+              setBrk(5);
               clock.current.innerText = 25 + ':00';
-              clearInterval(x)
+              setDist(0);
+              setOn(0);
+              clearInterval(x);
             }}/>
       </div>
       <div className="footer">
